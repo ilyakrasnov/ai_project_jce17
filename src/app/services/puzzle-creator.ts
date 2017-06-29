96aaabee7b3f77a1a3cfe9c65e970847ef289e60
Ilya <ilya.krasnov@gmail.com>
2017-06-29 19:43:09 +0300

    WIP puzzlecreator 2

--- src/app/services/puzzle-creator.ts
+++ src/app/services/puzzle-creator.ts
@@ -4,113 +4,153 @@ import { Http } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import { Book } from '../models/book';
 import * as _ from "lodash";
+import { MovementService } from '../services/movement';
 
+const times = x => f => {
+	if (x > 0) {
+		f()
+		times (x - 1) (f)
+	}
+}
 
 @Injectable()
 export class PuzzleCreatorService {
 
-  constructor(){
+	constructor(private movement: MovementService){
 
-  }
-  dimension;
+	}
 
-  // createPuzzle(dimension) {
-  //   this.dimension = dimension;
-  //
-  //   let board = this.createBoard();
-  //   let goalState = this.createGoalState()
-  //
-  //   console.log("###### BOARD CREATED ######");
-  //   console.log(board);
-  //
-  //   console.log("###### GOAL STATE ######");
-  //   console.log(goalState);
-  //
-  //   return [board, goalState];
-  // }
 
-  createBoard(dimension){
-    // this.dimension = dimension;
 
-    let range = _.range(1, dimension + 1);
-    let emptyBoard = this.emptyBoard(range);
-    return this.randomize(emptyBoard, dimension);
-  }
+	dimension;
 
-  createGoalState(dimension){
-    // this.dimension = dimension;
+	// createPuzzle(dimension) {
+	//   this.dimension = dimension;
+	//
+	//   let board = this.createBoard();
+	//   let goalState = this.createGoalState()
+	//
+	//   console.log("###### BOARD CREATED ######");
+	//   console.log(board);
+	//
+	//   console.log("###### GOAL STATE ######");
+	//   console.log(goalState);
+	//
+	//   return [board, goalState];
+	// }
 
-    let range = _.range(1, dimension + 1);
-    let emptyBoard = this.emptyBoard(range);
+	createBoard(dimension){
+		// this.dimension = dimension;
 
-    let nrOfCells = dimension ** 2;
-    let numbers = _.range(1, nrOfCells).concat([null]);
-    return this.assignNumbersToBoard(numbers, emptyBoard);
-  }
+		let range = _.range(1, dimension + 1);
+		let emptyBoard = this.emptyBoard(range);
 
-  randomize(board, dimension){
-    let nrOfCells = dimension ** 2;
-    let range = _.range(1, nrOfCells).concat([null]);
-    let randomNumbers = this.solvableSample(range, nrOfCells) ;
+		let originalBoard = this.createGoalState(dimension);
 
-    return this.assignNumbersToBoard(randomNumbers, board);
-  }
 
-  solvableSample(range, nrOfCells) {
-    let possibleSample = _.sampleSize(range, nrOfCells);
+		return this.randomize1(emptyBoard, dimension);
+		// return this.randomize2(originalBoard);
+	}
 
-    while (!this.solvable(possibleSample)) {
-      // console.log("###### PUZZLE UNSOLVABLE, redoing ######");
-      // console.log();
+	randomize2(board) {
+		console.log("###### GOT BOARD TO RANDOMIZE ######");
+		console.log(board);
 
-      possibleSample = _.sampleSize(range, nrOfCells);
-    }
+		let iterations = 10;
 
-    return possibleSample;
-  }
+		times (iterations)  (() => {
+			board = Object.assign([], this.makeRandomMove(board));
+		});
 
-  solvable(array) {
-    let totalInversions = 0;
-    for (var x of array) {
-      totalInversions = totalInversions + this.countInversions(x, array);
-    }
-    return this.isEven(totalInversions)
-  }
+		return board;
+	}
 
-  isEven(x){
-    return (x % 2) == 0;
-  }
-  
-  countInversions(x, array) {
-    let index = array.findIndex( el => el == x);
-    let tailAfterX = _.slice(array, index + 1);
-    return tailAfterX.map( el => el < x)
-        .reduce((a, b) => a + b, 0);
-  }
+	makeRandomMove(board){
+		let values = _.range(1, board.length);
+		let value = _.sample(values);
+		console.log("###### RANDOM VALUE: ######");
+		console.log(values);
+		return;
+	}
 
-  assignNumbersToBoard(numbers, board) {
-    let newBoard = Object.assign([], board);
-    _.each(numbers, (n, index) => {
-      newBoard[index] = Object.assign({}, newBoard[index], {
-        value: n
-      });
-    });
+	// play(board, value) {
+	// 	return this.movement.make_move(board, value);
+	// }
 
-    return newBoard;
-  }
+	createGoalState(dimension){
+		// this.dimension = dimension;
 
-  emptyBoard(range){
-    let board = [];
-    range.map( (y) => {
-      range.map( (x) => {
-        board.push({
-          x,
-          y,
-          value: null
-        })
-      })
-    })
-    return board;
-  }
+		let range = _.range(1, dimension + 1);
+		let emptyBoard = this.emptyBoard(range);
+
+		let nrOfCells = dimension ** 2;
+		let numbers = _.range(1, nrOfCells).concat([null]);
+		return this.assignNumbersToBoard(numbers, emptyBoard);
+	}
+
+	randomize1(board, dimension){
+		let nrOfCells = dimension ** 2;
+		let range = _.range(1, nrOfCells).concat([null]);
+		let randomNumbers = this.solvableSample(range, nrOfCells) ;
+
+		return this.assignNumbersToBoard(randomNumbers, board);
+	}
+
+	solvableSample(range, nrOfCells) {
+		let possibleSample = _.sampleSize(range, nrOfCells);
+
+		while (!this.solvable(possibleSample)) {
+			// console.log("###### PUZZLE UNSOLVABLE, redoing ######");
+			// console.log();
+
+			possibleSample = _.sampleSize(range, nrOfCells);
+		}
+
+		return possibleSample;
+	}
+
+	solvable(array) {
+		let totalInversions = 0;
+		for (var x of array) {
+			totalInversions = totalInversions + this.countInversions(x, array);
+		}
+		return this.isEven(totalInversions)
+	}
+
+	isEven(x){
+		return (x % 2) == 0;
+	}
+
+	countInversions(x, array) {
+		let index = array.findIndex( el => el == x);
+		let tailAfterX = _.slice(array, index + 1);
+		return tailAfterX.map( el => el < x)
+			.reduce((a, b) => a + b, 0);
+	}
+
+	assignNumbersToBoard(numbers, board) {
+		let newBoard = Object.assign([], board);
+		_.each(numbers, (n, index) => {
+			newBoard[index] = Object.assign({}, newBoard[index], {
+				value: n
+			});
+		});
+
+		return newBoard;
+	}
+
+	emptyBoard(range){
+		let board = [];
+		range.map( (y) => {
+			range.map( (x) => {
+				board.push({
+					x,
+					y,
+					value: null
+				})
+			})
+		})
+		return board;
+	}
 
 }
